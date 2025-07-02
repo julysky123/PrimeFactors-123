e967dec27224a7080f9859a061f8e68d8433ec91
MJ <julysky40@gmail.com>
2025-07-02 10:10:20 +0900

    [feature]62

--- PrimeFactorKata/prime-factors_test.cpp
+++ PrimeFactorKata/prime-factors_test.cpp
@@ -33,3 +33,8 @@ TEST_F(primeFactorsFixture, Of6) {
 	expected = {2,3};
 	EXPECT_EQ(expected, prime_factor.of(6));
 }
+
+TEST_F(primeFactorsFixture, Of9) {
+	expected = {3,3};
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
